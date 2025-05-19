8b8075a7407098d9e49cc261e74cb91254cba8b3
Turbo/ <burxells873@gmail.com>
2025-05-19 23:32:05 +0000

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+
+#[get("/hello")]
+async fn call() -> impl Responder {
+    HttpResponse::Ok().body("parcch!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!(" Server running at http://127.0.0.1:5000");
+    
+    HttpServer::new(|| {
+        App::new()
+            .service(call) // register route
+    })
+    .bind(("127.0.0.1", 5000))?
+    .run()
+    .await
+}
